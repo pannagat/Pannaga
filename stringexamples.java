b1b95d393a9cd0794a9a2d63b8202eecfdb411cd
pannagat <53312308+pannagat@users.noreply.github.com>
2020-07-23 21:08:53 +0530

    Add files via upload

---
+++ stringexamples.java
@@ -0,0 +1,48 @@
+class stringexamples
+{
+public static void main(String args[])
+{
+String s1="hello";
+String s2="meklo";
+String s3="keklo";
+System.out.println(s1.compareTo(s2));
+System.out.println(s1.compareTo(s3));
+
+System.out.println(s2.compareTo(s3));
+
+
+/*int i=s2.indexOf("bored");
+System.out.println(i);*/
+
+
+//System.out.println(s1.concat(s2));
+//System.out.println(s1.equals(s2))
+//System.out.println(s.charAt(4));
+//System.out.println(s.toUpperCase());
+//System.out.println(s.length());
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
